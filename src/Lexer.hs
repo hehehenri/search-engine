a7ee2bf79531c137c7627c278f5d435a9b717976
henri <henri@henr.in>
2023-11-23 18:42:29 -0300

    fix: change how doc is lexer

--- src/Lexer.hs
+++ src/Lexer.hs
@@ -6,18 +6,40 @@ import Debug.Trace
 
 data Token = Word String | Symbol String | Number String deriving(Show)
 
+nextToken :: String -> (Maybe Token, String)
+nextToken "" = (Nothing, "")
+nextToken buff@(c:cs)
+  | isSpace (trace "INFO: skipping space" c) = nextToken cs
+  | isAlpha (trace ("INFO: word init found: " ++ [c]) c) =
+            let (word, rest) = span isAlphaNum buff in
+            (Just (Lexer.Word word), rest)
+  | isDigit (trace ("INFO: number init found: " ++ [c]) c) =
+            let (num, rest) = span isDigit buff in
+            (Just (Lexer.Number num), rest)
+  | isSymbolChar (trace ("INFO: symbol init found: " ++ [c]) c) =
+            let (symb, rest) = span isSymbolChar buff in
+            (Just (Lexer.Symbol symb), rest)
+  | otherwise = error $ "ERROR: unexpected character: " ++ [c]
+
+-- TODO: does the order matters?
+appendToken :: Maybe Token -> [Token] -> [Token]
+appendToken token tokens =
+            case token of Just token -> token:tokens
+                          Nothing -> tokens
+
+appendNextToken :: String -> [Token] -> [Token]
+appendNextToken doc tokens =
+            let (token, doc) = nextToken doc in
+            let token = trace ("INFO: token found: " ++ show token) token in
+            let tokens = appendToken token tokens in
+            tokens
+
+tokenize' :: String -> [Token] -> [Token]
+tokenize' "" tokens = tokens
+tokenize' buff tokens =
+            let tokens = appendNextToken buff tokens in
+            tokenize' buff tokens
+
 tokenize :: String -> [Token]
-tokenize buffer@(c:cs)
-  | isAlpha (trace ("INFO: lexing alpha: " ++ [c]) c) =
-              let (word, rest) = span isAlphaNum buffer in
-              Lexer.Word word : tokenize rest
-  | isDigit (trace ("INFO: lexing digit: " ++ [c]) c) =
-              let (number, rest) = span isDigit buffer in
-              Lexer.Number number : tokenize rest
-  | isSymbolChar (trace ("INFO: lexing symbol:" ++ [c]) c) =
-              let (symbols, rest) = span isSymbolChar buffer in
-              Lexer.Symbol symbols : tokenize rest
-  | isSpace (trace "INFO: skipping space" c) =
-              let _ = traceId "INFO: skipping space" in
-              tokenize cs  
-  | otherwise = error $ "Invalid character: " ++ [c]
+tokenize doc = tokenize' doc []
+            
