6975d369a1fbd59e33b62cc4aff7a18caf10efb1
henri <henri@henr.in>
2023-11-26 00:11:42 -0300

    feat: use foldl to update token frequency

--- app/Main.hs
+++ app/Main.hs
@@ -7,18 +7,20 @@ import Data.Map as Map
 
 type FreqMap = Map String Int
 
-updateFreq :: [Lexer.Token] -> FreqMap -> FreqMap
-updateFreq [] freq = freq
-updateFreq (token:tokens) freq =
-  let newFreq = Map.insertWith (+) (show token) 1 freq in
-  updateFreq tokens newFreq
-
-genFreq :: [Lexer.Token] -> FreqMap
-genFreq t = updateFreq t Map.empty
+updateFreq :: [Lexer.Token] -> FreqMap
+updateFreq =
+  Prelude.foldl increment Map.empty
+  where
+    increment :: FreqMap -> Lexer.Token -> FreqMap 
+    increment freq token = Map.insertWith (+) (show token) 1 freq
+-- updateFreq [] freq = freq
+-- updateFreq (token:tokens) freq =
+--   let newFreq = Map.insertWith (+) (show token) 1 freq in
+--   updateFreq tokens newFreq
 
 main :: IO ()
 main = do
   doc <- Doc.parseUrl "https://www.haskell.org/"
   let tokens = Lexer.tokenize doc
-      freq = genFreq tokens in
+      freq = updateFreq tokens in
     putStr $ show freq
