953b88b07a39210b59adc7550ee974f19de29682
henri <henri@henr.in>
2023-11-30 12:57:19 -0300

    feat: i don't even know anymore

--- src/Crawler.hs
+++ src/Crawler.hs
@@ -8,18 +8,37 @@ import Text.HTML.TagSoup
 
 type Pages = Map.Map String [Tag String]
 
+parseUrl :: String -> Maybe String
+parseUrl ('h':'t':'t':'p':'s':':':'/':'/':url) = Just url
+parseUrl ('h':'t':'t':'p':':':'/':'/':url) = Just url
+parseUrl _ = Nothing
+
+getDomain :: String -> Maybe String
+getDomain url = do
+  url <- parseUrl url
+  let (domain, path) = break (== '/') url
+  return domain   
+
+fromSameDomain :: String -> String -> Bool
+fromSameDomain url url' =
+  case (getDomain url, getDomain url') of
+    (Just domain, Just domain') -> domain == domain'
+    (_, _) -> False
+
 updateUrls :: [String] -> [String] -> [String] -> [String]
-updateUrls  anchors urlsToVisit visitedUrls =
-  [url | url <- anchors, url `notElem` visitedUrls] ++ urlsToVisit
+updateUrls anchors urlsToVisit visitedUrls =
+  [url | url <- anchors, url `notElem` visitedUrls && fromSameDomain url (head visitedUrls)] ++ urlsToVisit
 
 traverse :: [String] -> Pages -> IO Pages
 traverse [] visitedPages = return visitedPages
 traverse (url:urlsToVisit) visitedPages = do
+  putStrLn $ "INFO: url: " ++ url
+  putStrLn $ "INFO: urlsToVisit: " ++ show urlsToVisit
   html <- fetchUrl url
   let tags = parseTags html 
       visitedPages = Map.insert url tags visitedPages 
-      anchors = extractAnchors tags 
-      urlsToVisit = updateUrls anchors  urlsToVisit $ Map.keys visitedPages in
+      anchors = extractAnchors tags
+      urlsToVisit = updateUrls anchors urlsToVisit $ Map.keys visitedPages in
     Crawler.traverse urlsToVisit visitedPages
 
 scan :: String -> IO Pages
