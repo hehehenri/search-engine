aa09bd7af4e3f7b856ef8dfab1b85628e38c3ee1
henri <henri@henr.in>
2023-12-07 14:28:14 -0300

    feat: fuck haskell

--- src/Http.hs
+++
@@ -1,8 +0,0 @@
-module Http(fetchUrl) where
-
-import Network.HTTP.Conduit (simpleHttp)
-
-fetchUrl :: String -> IO String
-fetchUrl url = do  
-  response <- simpleHttp url
-  return $ show response
