a14f3d3c80b4df51d265930b944a6b29fe685fbf
henri <henri@henr.in>
2023-12-12 11:17:31 -0300

    build: steal nix flake from teika (yoink)

--- flake.nix
+++ flake.nix
@@ -1,59 +1,28 @@
 {
+  description = "Search Engine Nix Flake";
+
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs";
+    nixpkgs.url = "github:anmonteiro/nix-overlays";
+    nix-filter.url = "github:numtide/nix-filter";
     flake-utils.url = "github:numtide/flake-utils";
-    ocaml-overlays.url = "github:nix-ocaml/nix-overlays";
-    ocaml-overlays.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, flake-utils, nixpkgs, ocaml-overlays }:
-    flake-utils.lib.eachDefaultSystem (system: 
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ ocaml-overlays.overlays.default ];
-        };
-      in with pkgs; rec {
-        devShells.default = mkShell {
-          inputsFrom = [ packages.default ];
-          buildInputs = [
-            ocamlPackages.merlin
-            ocamlformat
-            ocamlPackages.ocp-indent
-            ocamlPackages.utop
-          ];
-        };
-
-        packages.default = ocamlPackages.buildDunePackage rec {
-          pname = "search_engine";
-          version = "0.0.1";
-          useDune2 = true;
-          src = ./.;
-          buildInputs = with ocamlPackages; [ 
-            ppx_deriving
-            integers
-            piaf
-            data-encoding
-            utop
-            re
-            caqti
-            caqti-driver-postgresql
-            ppx_rapper
-            routes
-            jose
-            ppx_deriving_yojson
-            ppx_deriving_cmdliner
-          ];
-          nativeBuildInputs = [ makeBinaryWrapper ];
-
-          postFixup = ''
-            wrapProgram $out/bin/search_engine --prefix PATH : ${
-              lib.makeBinPath [ btrfs-progs openssh ]
-            }
-          '';
-
-          meta = { homepage = "https://github.com/hnrbs/search-engine"; };
-        };
+  outputs = { self, nixpkgs, nix-filter, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = (nixpkgs.makePkgs {
+        inherit system;
+        extraOverlays = [
+          (import ./nix/overlay.nix)
+        ];
+      }).extend (self: super: {
+        ocamlPackages = super.ocaml-ng.ocamlPackages_5_0;
+      }); in
+      let teika = pkgs.callPackage ./nix {
+        inherit nix-filter;
+        doCheck = true;
+      }; in
+      rec {
+        packages = { inherit teika; };
+        devShell = import ./nix/shell.nix { inherit pkgs teika; };
       });
-
 }
