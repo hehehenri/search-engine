ea046c0cd4e727a44af49e2988fcff2f6b1e3670
henri <henri@henr.in>
2023-12-22 01:31:54 -0300

    feat: index page

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,2 +1,38 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import { Search } from "lucide-svelte";
+
+  let url: string = "";
+  
+  const postIndex = () => {
+    console.log({ url });
+  };
+</script>
+
+<div class="w-screen h-screen flex items-center justify-center bg-indigo-50/40 min-w-[350px]">
+  <div class="w-full grid grid-rows-3 justify-center container mx-auto px-4 sm:px-6 lg:px-36">
+    <h1 class="text-center text-4xl sm:text-6xl font-chubbo font-bold text-indigo-400">
+      PoorMansGooggle
+    </h1>
+
+    <div class="flex flex-wrap justify-center mt-6 gap-x-8 gap-y-4 col-auto">
+      <div class="relative flex items-center w-80">
+        <input 
+          bind:value={url}
+          type="url"
+          placeholder="https://example.com/"
+          class="peer rounded-full placeholder:text-zinc-400 text-zinc-700 w-full px-3 py-1.5 pl-12 text-lg outline-indigo-300 shadow-[rgba(7,_65,_210,_0.1)_0px_9px_30px]"
+        />
+        <Search class="mx-3 absolute left-0 text-zinc-400 peer-focus:text-indigo-500" />
+      </div>
+
+      <div class="flex justify-center w-auto">
+        <button
+          on:click={postIndex}
+          class="block text-sm bg-indigo-500 px-4 py-2 rounded-full text-indigo-50 font-chubbo font-semibold"
+        >
+          Start Indexing
+        </button>
+      </div>
+    </div>
+  </div>
+</div>
---
+++ client/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    fontFamily: {
+      chubbo: ['Chubbo', 'sans-serif'],
+      supreme: ['Supreme', 'sans-serif'],
+      sans: ['Supreme', 'ui-sans-serif', 'system-ui'] ,
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
